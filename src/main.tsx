ed7c40c4d60cc42a68b3beb3636f9e5e3ff88c9b
ShashankJha2 <jha1shashank@gmail.com>
2026-01-26 16:41:57 -0500

    fix: remove repo prefix from index.html to fix vite build

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import "./styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
