4fc97e417e1fd8099ae9e9bbdc592e8d01fbe801
ShashankJha2 <jha1shashank@gmail.com>
2026-01-26 13:15:12 -0500

    fix: resolve path alias for build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 1. Add this import
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FBLA-LOSTANDFOUND/', 
+  base: '/FBLA-LOSTANDFOUND/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 2. Add this alias
+    },
+  },
 })
